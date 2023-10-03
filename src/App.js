53c00788f136ca8c52de274b08395768456d3375
Lee <leewhiz119@gmail.com>
2023-10-03 13:13:14 -0400

    started project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Demon from './Demon';
+import './App.css'
+
+
+export default function App() {
+  return (
+    <div className='App'>
+      <Demon />
+    </div>
+  )
+}
+
+
+
---
+++ src/Demon.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Demon() {
+  return (
+    <>
+    <div className="single_demon_header">
+      
+        <div className="back_btn">Back</div>
+      
+    </div>
+    <div className="single_demon_content"></div>
+    </>
+  )
+}
