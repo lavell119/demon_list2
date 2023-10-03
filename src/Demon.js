a28690c398e8969118949f8bb59d228943426d4b
Lee <leewhiz119@gmail.com>
2023-10-03 13:33:16 -0400

    added finalDemonsList2, set demons to Demon component state and rendered demon name based on arbitrary id value

--- src/Demon.js
+++ src/Demon.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import Demons from './finaldemonlist2.json'
+import { useState } from 'react'
 
 export default function Demon() {
+  const [demons, setDemons] = useState(Demons)
+  console.log(Demons)
+  const  id   = 3
   return (
     <>
     <div className="single_demon_header">
@@ -8,7 +13,7 @@ export default function Demon() {
         <div className="back_btn">Back</div>
       
     </div>
-    <div className="single_demon_content"></div>
+    <div className="single_demon_content">{demons[id].name}</div>
     </>
   )
 }
