dbc498ae58f7155f5d53a59aa0ee98e3a0547462
Lee <leewhiz119@gmail.com>
2023-10-07 17:00:51 -0400

    imported demons from finalDemonList.json set them to state, and mapped through them and rendered them as a list inside DemonList.js

--- src/DemonList.js
+++ src/DemonList.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import Demons from './finaldemonlist2.json'
+import { useState } from 'react';
+import { Link } from 'react-router-dom'
 
 export default function DemonList() {
-  return (
-    <div>DemonList</div>
-  )
+    const [demons, setDemons] = useState(Demons)
+    return (
+        <div>
+        {demons.map(demon=>
+            <div className="demon-snippet" key={demons.indexOf(demon)}>
+                <div className="snippet-left">
+                    <img src={ demon.image } alt="" />
+                    
+                  {/* <img src={ IMAGES.abbadon } alt="" /> */}
+                </div>
+                <div className="snippet-right">
+              <h3>
+                <Link className="white" to={`/demon/${demons.indexOf(demon)}`}>{demon.name}</Link>
+              </h3>
+              <p className="snippet-body">{demon.snippet}</p>
+              <span></span>
+              <Link class ="more-btn" to={`/demon/${demons.indexOf(demon)}`}>More &#x27A3;</Link>
+    
+                </div>
+            </div>
+            )}
+        </div>
+    )
 }
